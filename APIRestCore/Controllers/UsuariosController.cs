bf77bf89e1527f2a62d90c763df7298c12283d59
JoseMiguelDiezSen <josemigueldiezsen@gmail.com>
2021-03-26 11:59:00 +0100

    first commit

---
+++ APIRestCore/Controllers/UsuariosController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APIRestCore.Entities;
+using APIRestCore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace APIRestCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Usuarios usuarios = new Usuarios();
+            return Ok(usuarios.ObtenerUsuarios());
+        }
+
+        // READ
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            Usuarios usuarios = new Usuarios();
+            var usuario = usuarios.ObtenerUsuario(id);
+
+            if (usuario == null)
+            {
+                var notFound = NotFound("El usuario " + id.ToString() + " no existe.");
+                return notFound;
+            }
+
+            return Ok(usuario);
+        }
+
+        // CREATE
+        [HttpPost("Añadir")]
+        [SwaggerOperation(Summary = "Añade un usuario a la base de datos", Description = "Gets two hardcoded values")]
+        public IActionResult AgregarUsuario(Usuario nuevoUsuario)
+        {
+            Usuarios usuarios = new Usuarios();
+            usuarios.Agregar(nuevoUsuario);
+            return CreatedAtAction(nameof(AgregarUsuario), nuevoUsuario);
+        }
+
+        // UPDATE
+        [HttpPut("Actualizar")]
+        [SwaggerOperation(Summary = "Actualiza un usuario de la base de datos", Description = "Gets two hardcoded values")]
+        public IActionResult ActualizarUsuario(Usuario nuevoUsuario)
+        {
+            Usuarios usuarios = new Usuarios();
+            usuarios.Agregar(nuevoUsuario);
+            return CreatedAtAction(nameof(AgregarUsuario), nuevoUsuario);
+        }
+
+        // DELETE
+        [HttpDelete("Eliminar")]
+        [SwaggerOperation(Summary = "Elimina un usuario de la base de datos", Description = "Gets two hardcoded values")]
+        public IActionResult EliminarUsuario(int id)
+        {
+            Usuarios usuarios = new Usuarios();
+            usuarios.Eliminar(id);
+            return CreatedAtAction(nameof(AgregarUsuario), id);
+        }
+    }
+}
---
+++ APIRestCore/Models/Usuarios.cs
@@ -0,0 +1,45 @@
+﻿using APIRestCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace APIRestCore.Models
+{
+    public class Usuarios
+    {
+        public static List<Usuario> _listaUsuarios = new List<Usuario>()
+        {
+            new Usuario() { Id = 1, Nombre = "Cliente 1" , Apellido = "Apellido 1" },
+            new Usuario() { Id = 2, Nombre = "Cliente 2" , Apellido = "Apellido 2" },
+            new Usuario() { Id = 3, Nombre = "Cliente 3" , Apellido = "Apellido 3" }
+        };
+
+        public IEnumerable<Usuario> ObtenerUsuarios()
+        {
+            return _listaUsuarios;
+        }
+        
+        public Usuario ObtenerUsuario(int id)
+        {
+            var usuario = _listaUsuarios.Where(user => user.Id == id);
+            return usuario.FirstOrDefault();
+        }
+        
+        public void Agregar(Usuario nuevoUsuario)
+        {
+            _listaUsuarios.Add(nuevoUsuario);
+        }
+
+        public void Actualizar(int id)
+        {
+
+        }
+
+        public void Eliminar(int id)
+        {
+            _listaUsuarios.RemoveAt(id);
+        }
+
+    }
+}
+
